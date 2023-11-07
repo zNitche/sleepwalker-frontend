bdf9d7ef956b89a0cff9fb79f9c57b7a68f72b95
zNitche <53448802+zNitche@users.noreply.github.com>
2023-11-07 19:27:50 +0100

    added user settings to settings page

--- src/pages/settings/SettingsPage.tsx
+++ src/pages/settings/SettingsPage.tsx
@@ -1,15 +1,21 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
 import { Button, TextField } from "@mui/material"
 import FlexLoader from "../../components/flex_loader/FlexLoader"
 import "./styles.scss"
-import { httpGet, httpPost } from "../../utils/httpUtils"
+import { httpGet, httpPost, httpPut } from "../../utils/httpUtils"
+import { NotificationsContext } from "../../context/contexes"
+import { NotificationTypeEnum } from "../../enums/NotificationTypeEnum"
 
 
 export default function SettingsPage() {
+  let { addNotification } = useContext(NotificationsContext)
+
   const [isLoading, setIsLoading] = useState(true)
-  const [apiKeyLoading, setApiKeyLoading] = useState(false)
+  const [apiKeySettingsLoading, setApiKeySettingsLoading] = useState(false)
+  const [userSettingsLoading, setUserSettingsLoading] = useState(false)
 
   const [apiKey, setApiKey] = useState<string | null>(null)
+  const [hbPercentageThreshold, setHbPErcentageThreshold] = useState(0)
 
   useEffect(() => {
     setupData()
@@ -32,7 +38,7 @@ export default function SettingsPage() {
   }
 
   async function generateApiKey() {
-    setApiKeyLoading(true)
+    setApiKeySettingsLoading(true)
 
     const response = await httpPost("/auth/api-key/")
 
@@ -40,10 +46,10 @@ export default function SettingsPage() {
       setApiKey(response.data.api_key)
     }
 
-    setApiKeyLoading(false)
+    setApiKeySettingsLoading(false)
   }
 
-  function renderContent() {
+  function renderApiKeySettings() {
     return (
       <div className="section-wrapper">
         <div className="title">API Key Management</div>
@@ -56,11 +62,57 @@ export default function SettingsPage() {
           <Button
             type="submit"
             variant="contained"
-            disabled={apiKeyLoading}
+            disabled={apiKeySettingsLoading}
             onClick={generateApiKey}
           >Generate</Button>
         </div>
       </div>
+    )
+  }
+
+  async function saveUserSettings() {
+    setUserSettingsLoading(true)
+
+    const response = await httpPut("/auth/user/settings/", {
+      "sw_detection_heart_beat_percentage_threshold": hbPercentageThreshold
+    })
+
+    if (response.status == 200) {
+      addNotification("Successfully saved user settings", 2000, NotificationTypeEnum.Success)
+    } else {
+      addNotification("Error while saving user settings", 2000, NotificationTypeEnum.Error)
+    }
+
+    setUserSettingsLoading(false)
+  }
+
+  function renderUserSettings() {
+    return (
+      <div className="section-wrapper">
+        <div className="title">User Settings</div>
+        <div className="content api-key-wrapper">
+          <TextField
+            onChange={ (event) => {setHbPErcentageThreshold(event.target.value)}}
+            value={hbPercentageThreshold}
+            fullWidth
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={userSettingsLoading}
+            onClick={saveUserSettings}
+          >Save</Button>
+        </div>
+      </div>
+    )
+  }
+
+  function renderContent() {
+    return (
+      <>
+        {renderApiKeySettings()}
+        {renderUserSettings()}
+      </>
     )
   }
 
